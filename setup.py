c95b4462d4bbc3bca61c937b7ed9174cee461f97
Kaneesha Dawood <44208084+ktdawood@users.noreply.github.com>
2020-01-19 23:11:38 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using Twiiter data to identify the positive and negative sentiments in making a better purchasing decision.',
+    author='Kaneesha Dawood',
+    license='',
+)
